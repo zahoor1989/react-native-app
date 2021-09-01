138e7f95584e9edd00a331805228550efdc6978b
zahoor <zahoor_ahmed143@hotmail.com>
2021-09-01 08:39:07 +0400

    Flex box adddition

--- App.js
+++ App.js
@@ -1,23 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React, {useState} from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
-  const [outputText, setOutputText] = useState('Open up App.js to start working on your app!')
-  return (
-    <View style={styles.container}>
-      <Text>{outputText}</Text>
-      <Button  title="Display Name" onPress={() => setOutputText('Zahoor Ahmed Naqeebi')}/>
-      <StatusBar style="auto" />
+return (
+    <View style={{padding:50}}>
+    <View style={{flexDirection:'row', justifyContent:'space-between',alignItems:'center'}}>
+      <TextInput placeholder="Enter the values" style={{width:'80%',borderWidth:1,height:40, padding:10}}/>
+      <Button title="Add Task" style={{fontSize: 20, color: 'green'}}/>
+    </View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({ 
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
