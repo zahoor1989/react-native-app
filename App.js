974216921e2a75a9d80c11ada4dc882d223d69ed
zahoor_ahmed143@hotmail.com <zahoor_ahmed143@hotmail.com>
2021-09-01 09:05:23 +0400

    Flex box addition

--- App.js
+++ App.js
@@ -4,10 +4,15 @@ import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
 return (
-    <View style={{padding:50}}>
-    <View style={{flexDirection:'row', justifyContent:'space-between',alignItems:'center'}}>
-      <TextInput placeholder="Enter the values" style={{width:'80%',borderWidth:1,height:40, padding:10}}/>
-      <Button title="Add Task" style={{fontSize: 20, color: 'green'}}/>
+    <View  style={{paddingTop:50,width:'100%',height:300,flexDirection:'row'}}>
+    <View  style={{flex:1,backgroundColor:'red',alignItems:'center',justifyContent:'center'}}>
+      <Text>1</Text>
+    </View>
+    <View  style={{flex:1, backgroundColor: 'blue',justifyContent:'center',alignItems:'center'}}>
+      <Text>2</Text>
+    </View>
+    <View  style={{flex:1, backgroundColor:'green',justifyContent:'center',alignItems:'center'}}>
+      <Text>3</Text>
     </View>
     </View>
   );
