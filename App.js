374ebeb032af36516631d37c1c326a2323795ac9
Zahoor Ahmed <zahoor_ahmed143@hotmail.com>
2021-09-02 09:00:37 +0400

    flex box creation

--- App.js
+++ App.js
@@ -1,19 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
-return (
-    <View  style={{paddingTop:50,width:'100%',height:300,flexDirection:'row'}}>
-    <View  style={{flex:1,backgroundColor:'red',alignItems:'center',justifyContent:'center'}}>
-      <Text>1</Text>
-    </View>
-    <View  style={{flex:1, backgroundColor: 'blue',justifyContent:'center',alignItems:'center'}}>
-      <Text>2</Text>
-    </View>
-    <View  style={{flex:1, backgroundColor:'green',justifyContent:'center',alignItems:'center'}}>
-      <Text>3</Text>
-    </View>
+  return (
+    <View style={styles.screen}>
+      <View style={styles.textContainer1}>
+        <Text>1</Text>
+      </View>
+      <View style={styles.textContainer2}>
+        <Text>2</Text>
+      </View>
+      <View style={styles.textContainer3}>
+        <Text>3</Text>
+      </View>
     </View>
   );
-}
+};
+const textContainer = { flex: 1,alignItems: 'center', justifyContent: 'center' }
+const styles = StyleSheet.create({
+  screen: {
+    paddingTop: 50,
+    width: '100%',
+    height: 300,
+    flexDirection: 'row'
+  },
+  textContainer1 : { ...textContainer,backgroundColor:'red'},
+  textContainer2: {
+    ...textContainer,
+    backgroundColor:'green'
+  },
+  textContainer3 :{
+    ...textContainer,
+    backgroundColor:'purple'
+  }
+
+})
