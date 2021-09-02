fb791a8dc7602e7c0d1ff74891db61fea34d5830
Zahoor Ahmed <zahoor_ahmed143@hotmail.com>
2021-09-02 09:14:33 +0400

    styling for text inputs and view

--- App.js
+++ App.js
@@ -5,34 +5,31 @@ import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 export default function App() {
   return (
     <View style={styles.screen}>
-      <View style={styles.textContainer1}>
-        <Text>1</Text>
-      </View>
-      <View style={styles.textContainer2}>
-        <Text>2</Text>
-      </View>
-      <View style={styles.textContainer3}>
-        <Text>3</Text>
+      <View style={styles.textContainer}>
+        <TextInput placeholder="Enter Course" style={styles.inputContainer}/>
+        <Button title="ADD"/>
       </View>
     </View>
   );
 };
-const textContainer = { flex: 1,alignItems: 'center', justifyContent: 'center' }
 const styles = StyleSheet.create({
   screen: {
-    paddingTop: 50,
-    width: '100%',
-    height: 300,
-    flexDirection: 'row'
+    paddingTop:50,
+    paddingRight:20,
+    paddingLeft:20,
+    paddingBottom:20
   },
-  textContainer1 : { ...textContainer,backgroundColor:'red'},
-  textContainer2: {
-    ...textContainer,
-    backgroundColor:'green'
-  },
-  textContainer3 :{
-    ...textContainer,
-    backgroundColor:'purple'
-  }
+  textContainer: { 
+    flexDirection: 'row',
+    justifyContent:'space-between',
+    alignItems:'center'
+   },
+   inputContainer:{
+     width:'80%',
+     padding:5,
+     borderWidth:2,
+     borderColor:'black'
+   }
+
 
 })
